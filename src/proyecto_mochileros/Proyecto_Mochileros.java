44787c2e51aff5c2cce75f1b07a802206916896f
Porfirio <91766266+Porfirio1@users.noreply.github.com>
2021-11-23 14:36:40 -0600

    Update Proyecto_Mochileros.java
    Agrege algo

--- src/proyecto_mochileros/Proyecto_Mochileros.java
+++ src/proyecto_mochileros/Proyecto_Mochileros.java
@@ -1,19 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto_mochileros;
 
-/**
- * Holaaaaaa 
- */
 public class Proyecto_Mochileros {
 
     
     public static void main(String[] args) {
-        // hi
+        
         System.out.println("Lord Hokage");
+        System.out.println("Lord Hokage se murio ");
     }
     
 }
